e00fe6ddb135377df248036c91d8e6a1510fd34f
andrey <andreyksu@gmail.com>
2017-02-05 21:14:41 +0300

    Добавил первые Unit тесты. TODO: Нужно настроить мавет на автоматический прогон тестов.

---
+++ com.gmail.andreyksu.guiproject/scr/test/java/com/gmail/andreyksu/guiproject/TestCalc.java
@@ -0,0 +1,36 @@
+package com.gmail.andreyksu.guiproject;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+import com.gmail.andreyksu.modelpack.pefrormcalc.polishreversenotation.CalculatorWithRPN;
+
+public class TestCalc {
+    
+    @Test
+    public void firstPassTestRPNCalc() throws Exception{
+        String expected = "4.0";
+        CalculatorWithRPN calcRPN = new CalculatorWithRPN();
+        String result = calcRPN.performingCalculations("-(2*(-08.0+2.0*(-5))/(-1+3*(-2)-(-4)))+4*2.0^2");
+        assertArrayEquals(expected.toCharArray(), result.toCharArray());
+    }
+    
+    @Test
+    public void secondPassTestRPNCalc() throws Exception{
+        String expected = "6.947368421";
+        CalculatorWithRPN calcRPN = new CalculatorWithRPN();
+        String tmpResult = calcRPN.performingCalculations("-(22*(-08.0+2.0*(-5))/(-1+3*(-2)-(-4^3)))");
+        String result = tmpResult.substring(0, 11);
+        assertArrayEquals(expected.toCharArray(), result.toCharArray());
+    }
+    
+    @Test
+    public void therdPassTestRPNCalc() throws Exception{
+        String expected = "6.0";
+        CalculatorWithRPN calcRPN = new CalculatorWithRPN();
+        String result = calcRPN.performingCalculations("2+2*2");
+        assertArrayEquals(expected.toCharArray(), result.toCharArray());
+    }
+    
+
+}
